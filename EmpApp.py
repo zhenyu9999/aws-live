3d6a7a862702d6832250d8137cd55a06f885e65a
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-21 00:21:04 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -26,7 +26,7 @@ def home():
     return render_template('AddEmp.html')
 
 
-@app.route("/about", methods=['GET', 'POST'])
+@app.route("/about", methods=['POST'])
 def about():
     return render_template('about.html')
 
