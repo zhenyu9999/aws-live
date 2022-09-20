2c7ba06911236a45048e172218fb1a2cf2ca9de0
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-21 01:20:36 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for,session,flash
 from pymysql import connections
 import os
 import boto3
