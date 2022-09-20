15bdfb6fff84bad68d1ec96e847922c19f4e18dc
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-20 23:43:21 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -28,7 +28,7 @@ def home():
 
 @app.route("/about", methods=['POST'])
 def about():
-    return render_template('www.intellipaat.com')
+    return render_template('about.html')
 
 
 @app.route("/addemp", methods=['POST'])
