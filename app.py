f032c855b82d6dc48f3f2abd08e9e6faee2c6a9f
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-21 14:44:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def view_assign():
     return render_template("view_assign.html",assign=assign)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
