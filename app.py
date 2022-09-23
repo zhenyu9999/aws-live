3694691a4b7dd00b839d0a3329c3c1dc7c1cc4ad
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-23 20:57:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask, render_template, request, redirect, url_for,session
-from flask_sqlalchemy import SQLAlchemy
 from pymysql import connections
 import os
 import boto3
