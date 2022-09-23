79d4c1c20dc8065ac2b07dce806d73507076c1af
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-23 20:48:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,121 +20,49 @@ db_conn = connections.Connection(
     db=customdb
 
 )
-
-@app.route("/department_assign")
-def d():
-    department=department.query.all()
-    employe=employees.query.all()
-    role= role.query.all()
-    return render_template("assign_department.html",role=role,department=department,employe=employe)
-
-@app.route("/assign_department",methods=['GET','POST'])
-def assign_department():
-    if request.method == 'POST':
-        employe= request.form.get("employe")
-        department= request.form.get("department")
-        role= request.form.get("role")
-        add = assign_department(employee=employe,department=department,role=role)
-        db.session.add(add)
-        db.session.commit()
-        return redirect("/department_assign")
+output = {}
+table = 'employee'
 
 
 @app.route("/")
-def ad():
-    if session.get("username"):
+def start():
+    if session.get("admin_id"):
         return render_template("dashboard.html")
     else:
         return render_template("login.html")
 
 
-@app.route("/update/<int:id>",methods=['GET','POST'])
-def update(id):
-    em = employee.query.get(id)
-    if request.method =="POST":
-        em.email = request.form['email']
-        em.name = request.form['name']
-        em.qualification = request.form['qualification']
-        em.phone = request.form['phone']
-        db.session.commit()
-        return redirect("/employee")
-    return render_template("update.html",em=em)
-
-@app.route("/delete/<int:id>",methods=['GET','POST'])
-def delete(id):
-    em=employee.query.filter_by(id=id).first()
-    db.session.delete(em)
-    db.session.commit()
-    return redirect("/employee")
-
-
-
 @app.route("/login",methods=['GET','POST'])
 def login():
 
-    user = register.query.all()
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-      
-        use= register.query.filter_by(username=username,password=password).first()
-        if use:
-            
-            session["username"]=use.username
-            return redirect("/")
-        
+   def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'admin_name' in request.form and 'admin_password' in request.form:
+        # Create variables for easy access
+        admin_name = request.form['admin_name']
+        admin_password = request.form['admin_password']
+        # Check if account exists using MySQL
+        validateAccount = 'SELECT * FROM admin WHERE username = %s AND password = %s', (admin_name, admin_password,)
+        cursor = db_conn.cursor()
+        cursor.execute(validateAccount)
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['admin_id'] = account['admin_id']
+            session['admin_name'] = account['admin_name']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('login.html', msg=msg)
 
-    return render_template("login.html")
-
-@app.route("/register",methods=['GET','POST'])
-def register():
-
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        first_name = request.form.get("first_name")
-        last_name = request.form.get("last_name")
-        birthday = request.form.get("birthday")
-        gender = request.form.get("gender")
-        
-        add=register(first_name=first_name,last_name=last_name,password=password,username=username,birthday=birthday,gender=gender)
-        db.session.add(add)
-        db.session.commit()
-        return redirect("/")
-    return render_template("register.html")
-
-@app.route("/add",methods=['GET','POST'])
-def add():
-    if session.get("username"):
-        if request.method =="POST":
-            name=request.form.get("name")
-            email=request.form.get("email")
-            qualification=request.form.get("qualification")
-            phone=request.form.get("phone")
-            add = Employees(name=name,email=email,qualification=qualification,phone=phone)
-            db.session.add(add)
-            db.session.commit()
-            return redirect("/add")
-    else:
-        return redirect("/login")
-    return render_template("add.html")
-
-@app.route("/logout")
-def logout():
-    session["username"]=[]
-    return redirect("/login")
-    
-@app.route("/employee",methods=['GET','POST'])
-def employe():
-    if session.get("username"):
-        employe=employees.query.all()
-        return render_template("employees.html",employe=employe)
-    else:
-        return render_template(login.html)
-@app.route("/view_assign",methods=['GET','POST'])
-def view_assign():
-    assign= assign_department.query.all()
-    return render_template("view_assign.html",assign=assign)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
